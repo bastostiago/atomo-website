8b6b97ca19ff2c1153956642acfbcaed96eb3675
Tiago Bastos <38707338+bastostiago@users.noreply.github.com>
2022-09-29 17:40:21 -0300

    only one html file; handling with scroll

---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+  
+    });
+
+    // Animation image on welcome
+    $("#mainImage").fadeIn(1000);
+
+    //Select current item on NavMenu
+    const sections = document.querySelectorAll("section");
+    const navA = document.querySelectorAll("nav .navbar-item");
+    window.onscroll = () => {
+        var current = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= sectionTop - 60) {
+                current = section.getAttribute("id");                 
+            }
+        });
+
+        navA.forEach((a) => {                   
+            a.classList.remove("is-active");
+            // console.log(current);
+            // console.log('ClassList: ' + a.classList);
+            if (a.href.includes(current)) {
+                a.classList.add("is-active");
+            }
+        });
+    };
+
+
+
+  });
